ee3df609b5e69bcd42f2668c3ebf443b0a3139b1
Jeff <YZJ_JEFF@163.COM>
2020-04-24 19:03:19 +0800

    update and change2

--- src/java/GitDemo.java
+++ src/java/GitDemo.java
@@ -2,6 +2,6 @@ package java;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("git change");
+        System.out.println("update git and change");
     }
 }
