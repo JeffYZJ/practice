77f78529efd5f13deff7066581fe736d84f78f2f
Jeff <Email>
2020-04-25 10:39:40 +0800

    Default Changelist

--- src/java/GitDemo.java
+++ src/java/GitDemo.java
@@ -4,6 +4,8 @@ public class GitDemo {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
+        String string = "Test";
+        String nextStr = "Commit进行对比哈哈哈";
         System.out.println("int number");
     }
 }
